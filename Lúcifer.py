4efd48bc25e3fb0898513d4c857882ae1eef9453
Juan Valença de Oliveira <starbea2405@gmail.com>
2024-12-04 13:40:32 -0300

    Add files via upload

---
+++ Lúcifer.py
@@ -0,0 +1,36 @@
+Breeds = [
+    {"Raça": "Humano", "Nome": "Chloe Decker", "Idade": "39 - 40 anos (78 anos timeskip)", "O que é": "Detetive policial", "Status": "Morta"},
+    {"Raça": "Humano", "Nome": "Dan Espinoza", "Idade": "39 - 40 anos", "O que é": "Detetive policial até o fim da 2° temporada", "Status": "Encontrou a paz e está no céu"},
+    {"Raça": "Humano", "Nome": "Beatrice Espinoza (Trixie)", "Idade": "13 anos (52 - 53 após timeskip)", "O que é": "Astronauta pós timeskip", "Status": "Viva"},
+    {"Raça": "Humano", "Nome": "Linda Martin", "Idade": "42 - 43 anos (82 - 83 anos timeskip)", "O que é": "Psicóloga", "Status": "Viva"},
+    {"Raça": "Humano", "Nome": "Ella Lopez", "Idade": "33 - 34 anos (73 - 74 anos timeskip)", "O que é": "CSI", "Status": "Viva"},
+    {"Raça": "Humano", "Nome": "Malcom Grahm", "Idade": "42 anos", "O que é": "Policial (Corrupto)", "Status": "Morto"},
+    {"Raça": "Humano", "Nome": "Caim", "Idade": "Milhões de anos", "O que é": "Capitão de Policia", "Status": "Morto"},
+    {"Raça": "Humano", "Nome": "Eva", "Idade": "Milhões de anos (mais do que Caim)", "O que é": "Nada", "Status": "Viva"},
+    {"Raça": "Anjo", "Nome": "Lúcifer Morningstar(Samael)", "Idade": "+13,8 Bilhões de anos", "O que é": "Terapeuta no Inferno", "Status": "Vivo"},
+    {"Raça": "Anjo", "Nome": "Amenadiel", "Idade": "+13,8 Bilhões de anos", "O que é": "Deus e anteriormente Arcanjo", "Status": "Vivo"},
+    {"Raça": "Anjo", "Nome": "Miguel", "Idade": "+13,8 Bilhões de anos", "O que é": "Arcanjo", "Status": "Vivo"},
+    {"Raça": "Anjo", "Nome": "Azrael", "Idade": "+13,8 Bilhões de anos", "O que é": "Anjo da Morte", "Status": "Vivo"},
+    {"Raça": "Anjo", "Nome": "Uriel", "Idade": "+13,8 Bilhões de anos", "O que é": "Anjo", "Status": "Obliterado da existência"}
+]
+
+
+for breed in Breeds:
+    print(f"Nome: {breed['Nome']}, Raça: {breed['Raça']}, Status: {breed['Status']}")
+
+
+def escolha(persona_nome):
+    for breed in Breeds:
+        if breed["Nome"].lower() == persona_nome.lower():
+            print("\nPersonagem encontrado:")
+            print(f"Raça: {breed['Raça']}")
+            print(f"Nome: {breed['Nome']}")
+            print(f"Idade: {breed['Idade']}")
+            print(f"O que é: {breed['O que é']}")
+            print(f"Status: {breed['Status']}")
+            return
+    print("\nPersonagem não encontrado.")
+
+
+persona = input("\nEscolha seu personagem pelo nome: ")
+escolha(persona)
